453b582c850720113e515f6718325d58993cfc46
Daniel H <dhenkes@icloud.com>
2019-10-03 15:25:42 +0000

    Add randomColor function

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ var red;
 var green;
 var blue;
 var rgbColor;
+function randomColor () {
+  return Math.floor(Math.random() * 256 );
+}
 
-for ( i = 0; i < 10; i += 1) {
-red = Math.floor(Math.random() * 256 );
-green = Math.floor(Math.random() * 256 );
-blue = Math.floor(Math.random() * 256 );
+for ( i = 0; i < 10000; i += 1) {
+red = randomColor();
+green = randomColor();
+blue = randomColor();
 rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
 html += '<div style="background-color:' + rgbColor + '"></div>';
 }
